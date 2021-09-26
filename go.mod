4664e6da2ebc533dd68cbe13104041b1cd6e009f
akez <1791189210@qq.com>
2021-09-26 11:56:34 +0800

    chromedp + goquery

--- go.mod
+++ go.mod
@@ -5,17 +5,16 @@ go 1.16
 require (
 	github.com/MontFerret/ferret v0.15.0
 	github.com/andybalholm/cascadia v1.2.0 // indirect
-	github.com/chromedp/cdproto v0.0.0-20210921215903-b0b4414ddbe0 // indirect
-	github.com/chromedp/chromedp v0.7.4 // indirect
+	github.com/chromedp/cdproto v0.0.0-20210921215903-b0b4414ddbe0
+	github.com/chromedp/chromedp v0.7.4
 	github.com/influxdata/influxdb-client-go/v2 v2.5.1
 	github.com/influxdata/line-protocol v0.0.0-20210311194329-9aa0e372d097 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
-	github.com/robfig/cron/v3 v3.0.1
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/spf13/viper v1.9.0
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common v1.0.257
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms v1.0.257
-	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d
 	golang.org/x/sys v0.0.0-20210925032602-92d5a993a665 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
--- main.go
+++ main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"github.com/MontFerret/ferret/pkg/compiler"
-	"github.com/MontFerret/ferret/pkg/drivers"
-	"github.com/MontFerret/ferret/pkg/drivers/cdp"
-	"github.com/MontFerret/ferret/pkg/drivers/http"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
+	"io/ioutil"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -36,30 +34,57 @@ func main() {
 func generatePDF(pre string) {
 	ctx, cancel := chromedp.NewExecAllocator(context.Background(), append(
 		chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", false),
+		chromedp.Flag("headless", true),
 	)...)
 	ctx, cancel = chromedp.NewContext(ctx)
+	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
 	defer cancel()
 
-	// capture pdf
-	var outerBefore string
+	var buf []byte
+	var htmlContent string
+
 	if err := chromedp.Run(ctx,
-		chromedp.Navigate(`https://s.weibo.com/top/summary?cate=realtimehot`),
-		chromedp.Sleep(10*time.Second),
-		chromedp.OuterHTML("#pl_top_realtimehot", &outerBefore),
+		printToPDF(`https://s.weibo.com/top/summary?cate=realtimehot`, &buf),
+		getHtmlContent(`https://s.weibo.com/top/summary?cate=realtimehot`, &htmlContent),
 	); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(outerBefore)
+	if err := ioutil.WriteFile("sample.pdf", buf, 0644); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(htmlContent)
 }
 
-func printToPDF(urlstr string, res *[]byte) chromedp.Tasks {
+func getHotSearchData(htmlContent string) {
+	dom, err := goquery.NewDocumentFromReader(strings.NewReader(htmlContent))
+	if err != nil {
+		log.Println("new dom error")
+	}
+	dom.Find("").Each(func(i int, selection *goquery.Selection) {
 
+	})
+
+}
+
+func getHtmlContent(url string, html *string) chromedp.Tasks {
 	return chromedp.Tasks{
-
+		//chromedp.Emulate(device.IPhone8),
+		chromedp.Navigate(url),
+		chromedp.WaitReady("div#pl_top_realtimehot", chromedp.ByQuery),
+		chromedp.OuterHTML("div#pl_top_realtimehot", html),
 		chromedp.ActionFunc(func(ctx context.Context) error {
+			return nil
+		}),
+	}
+}
 
-			buf, _, err := page.PrintToPDF().WithPrintBackground(false).Do(ctx)
+func printToPDF(url string, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		//chromedp.Emulate(device.IPhone8),
+		chromedp.Navigate(url),
+		chromedp.WaitReady("div#pl_top_realtimehot", chromedp.ByQuery),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			buf, _, err := page.PrintToPDF().WithPaperWidth(7).WithPaperHeight(21).WithPrintBackground(false).WithPreferCSSPageSize(true).Do(ctx)
 			if err != nil {
 				return err
 			}
@@ -67,69 +92,4 @@ func printToPDF(urlstr string, res *[]byte) chromedp.Tasks {
 			return nil
 		}),
 	}
-}
-
-type HotSearch struct {
-	Rank    int64  `json:"rank"`
-	Content string `json:"content"`
-	Link    string `json:"link"`
-	Hot     int64  `json:"hot"`
-	Tag     string `json:"tag"`
-}
-
-func getHotSearch() []*HotSearch {
-	query := `
-			LET doc = DOCUMENT("https://s.weibo.com/top/summary?cate=realtimehot", {driver: "cdp"})
-
-			WAIT_NAVIGATION(doc, 7000)
-
-			LET data = ELEMENTS(doc, "div#pl_top_realtimehot > table > tbody > tr")
-			LET realData = (
-				FOR el IN data
-					FILTER TO_INT(el[0].innerText) > 0
-					LET rank = TO_INT(el[0].innerText)
-					LET content = el[1][0].innerText
-					LET link = el[1][0].attributes.href
-					LET hot = TO_INT(el[1][1].innerText)
-					LET tag = el[2].innerText
-
-					RETURN {
-						rank: rank, 
-						content: content,
-						link: link,
-						hot: hot,
-						tag: tag,
-					}
-			)
-			
-			RETURN realData
-		`
-
-	comp := compiler.New()
-
-	program, err := comp.Compile(query)
-
-	if err != nil {
-		log.Fatal("compile error")
-	}
-
-	ctx := context.Background()
-	ctx = drivers.WithContext(ctx, cdp.NewDriver())
-	ctx = drivers.WithContext(ctx, http.NewDriver(), drivers.AsDefault())
-
-	out, err := program.Run(ctx)
-
-	if err != nil {
-		log.Fatal("run error")
-	}
-
-	res := make([]*HotSearch, 0)
-
-	err = json.Unmarshal(out, &res)
-
-	if err != nil {
-		log.Fatal("json error")
-	}
-
-	return res
 }
